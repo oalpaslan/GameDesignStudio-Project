f9ae0233de9a36799cb602631d05c22e25bba1fd
Oğuzhan Alpaslan <37627249+oalpaslan@users.noreply.github.com>
2024-04-20 17:52:39 +0300

    movement, jump, no double jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,11 +9,18 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float jumpForce = 5f;
 
+    private bool isOnGround;
+    public Transform groundCheckPoint;
+    public LayerMask whatIsGround;
+
+
     public Rigidbody2D rBody;
+
+    public SpriteRenderer pRenderer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        pRenderer = gameObject.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -21,11 +28,24 @@ public class PlayerController : MonoBehaviour
     {
         rBody.velocity = new Vector2(pSpeed * Input.GetAxis("Horizontal"), rBody.velocity.y);
 
-        if(Input.GetButtonDown("Jump"))
+        isOnGround = Physics2D.OverlapCircle(groundCheckPoint.position, .05f, whatIsGround); //OverlapCircle tells if a circle in a position overlaps with another collider
+
+
+        if (Input.GetButtonDown("Jump") && isOnGround)
         {
 
             rBody.velocity = new Vector2(rBody.velocity.x, jumpForce);
 
         }
+
+        if(Input.GetAxis("Horizontal") < 0)
+        {
+            pRenderer.flipX = true;
+        }
+        else if(Input.GetAxis("Horizontal") > 0)
+        {
+            pRenderer.flipX = false;
+        }
+
     }
 }
