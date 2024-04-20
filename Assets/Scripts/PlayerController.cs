0163d36dc1e9eb8411a9f63dd0fee7de91dbda64
Oğuzhan Alpaslan <37627249+oalpaslan@users.noreply.github.com>
2024-04-20 21:51:25 +0300

    wall jump, wall slide

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,75 +8,152 @@ public class PlayerController : MonoBehaviour
     private float pSpeed = 8f;
     [SerializeField]
     private float jumpForce = 5f;
+    [SerializeField]
+    private float wallSlidingSpeed = 2f;
 
-    private bool isOnGround;
-    public Transform groundCheckPoint;
+    private bool isOnGround, isOnWall;
+    private bool isWallSliding;
+    public Transform groundCheckPoint, wallCheckPoint;
     public LayerMask whatIsGround;
+    public LayerMask whatIsWall;
+
+    private bool isFacingRight = true;
 
 
     public Rigidbody2D rBody;
-
+    public CapsuleCollider2D pCollider;
     public SpriteRenderer pRenderer;
 
     public float collisionRayLength = 0.5f;
+
+    private Animator anim;
+
+    public int curBlood, maxBlood;
+    public bool soulMate;
+
+
+    private bool isWallJumping;
+    private float wallJumpingDirection;
+    private float wallJumpingTime = 0.2f;
+    private float wallJumpingCounter;
+    private float wallJumpingDuration = 0.4f;
+    private Vector2 wallJumpingPower = new Vector2(8f, 16f);
+
     // Start is called before the first frame update
     void Start()
     {
         pRenderer = gameObject.GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+        pCollider = GetComponent<CapsuleCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        DetectCollision();
         rBody.velocity = new Vector2(pSpeed * Input.GetAxis("Horizontal"), rBody.velocity.y);
+        anim.SetFloat("Speed", Mathf.Abs(rBody.velocity.x));
+        anim.SetBool("IsWallSlide", isWallSliding);
+        anim.SetBool("IsOnGround", isOnGround);
 
         isOnGround = Physics2D.OverlapCircle(groundCheckPoint.position, .05f, whatIsGround); //OverlapCircle tells if a circle in a position overlaps with another collider
 
+        isOnWall = Physics2D.OverlapCircle(wallCheckPoint.position, .1f, whatIsWall);
+
+        WallSlide();
+        WallJump();
 
         if (Input.GetButtonDown("Jump") && isOnGround)
         {
-
             rBody.velocity = new Vector2(rBody.velocity.x, jumpForce);
-
         }
 
         if(Input.GetAxis("Horizontal") < 0)
         {
             pRenderer.flipX = true;
+            wallCheckPoint.transform.position = new Vector2(gameObject.transform.position.x - 0.2f, transform.position.y);
+            wallCheckPoint.transform.rotation = new Quaternion(0, 180, 0,0);
         }
         else if(Input.GetAxis("Horizontal") > 0)
         {
             pRenderer.flipX = false;
+            wallCheckPoint.transform.position = new Vector2(gameObject.transform.position.x + 0.2f, transform.position.y);
+            wallCheckPoint.transform.rotation = new Quaternion(0, 0, 0, 0);
+
         }
 
     }
 
-    private void DetectCollision()
+    private void VampireSpeed()
     {
-        RaycastHit2D hitLeft = Physics2D.Raycast(transform.position, Vector2.left, collisionRayLength);
-        RaycastHit2D hitRight = Physics2D.Raycast(transform.position, Vector2.right, collisionRayLength);
-        RaycastHit2D hitUp = Physics2D.Raycast(transform.position, Vector2.up, collisionRayLength);
-        RaycastHit2D hitDown = Physics2D.Raycast(transform.position, Vector2.down, collisionRayLength);
+        
+    }
 
-        if (hitLeft.collider != null && hitLeft.collider.CompareTag("Wall"))
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Object"))
         {
-            Debug.Log("Collided with left side of the tile");
-        }
+            GameObject obj = GameObject.FindGameObjectWithTag("Object");
 
-        if (hitRight.collider != null && hitRight.collider.CompareTag("Wall"))
-        {
-            Debug.Log("Collided with right side of the tile");
-        }
+            obj.transform.Translate(new Vector2(1,0) * Time.deltaTime);
 
-        if (hitUp.collider != null && hitUp.collider.CompareTag("Wall"))
-        {
-            Debug.Log("Collided with top side of the tile");
-        }
-
-        if (hitDown.collider != null && hitDown.collider.CompareTag("Wall"))
-        {
-            Debug.Log("Collided with bottom side of the tile");
         }
     }
+
+    private void WallJump()
+    {
+        if(isWallSliding)
+        {
+            isWallJumping = false;  
+            wallJumpingDirection = -transform.localRotation.x;
+            wallJumpingCounter = wallJumpingTime;
+            
+            CancelInvoke(nameof(StopWallJumping));
+        }
+        else
+        {
+            wallJumpingCounter -= Time.deltaTime;
+        }
+        if (Input.GetButtonDown("Jump") && wallJumpingCounter > 0f)
+        {
+            isWallJumping= true;
+            rBody.velocity = new Vector2(wallJumpingDirection * wallJumpingPower.x, wallJumpingPower.y);
+            wallJumpingCounter = 0f;
+
+            if(transform.localScale.x != wallJumpingDirection)
+            {
+                pRenderer.flipX=!pRenderer.flipX;
+            }
+
+            Invoke(nameof(StopWallJumping), wallJumpingDuration);
+        }
+    }
+
+    private void StopWallJumping()
+    {
+        isWallJumping=false;
+    }
+
+    private void WallSlide()
+    {
+        if(isOnWall && !isOnGround)
+        {
+            isWallSliding = true;
+            rBody.velocity = new Vector2(rBody.velocity.x, 0);
+        }
+        else
+        {
+            isWallSliding = false;
+        }
+    }
+
+    //private void Flip()
+    //{
+    //    if(isFacingRight && Input.GetAxisRaw("Horizontal") < 0f || !isFacingRight && Input.GetAxisRaw("Horizontal") > 0f)
+    //    {
+    //        isFacingRight = !isFacingRight;
+    //        Vector3 localScale = transform.localScale;
+    //        localScale.x *= -1f;
+    //        transform.localScale = localScale;
+    //    }
+    //}
 }
